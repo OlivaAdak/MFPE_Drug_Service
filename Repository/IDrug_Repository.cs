8e29398f2eb38c3a352b8469714841be07f6c0d0
OlivaAdak <oliva.adak@gmail.com>
2020-09-29 12:35:31 +0530

    Source code added

---
+++ Repository/IDrug_Repository.cs
@@ -0,0 +1,16 @@
+﻿using MailOrderPharmacy_DrugService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailOrderPharmacy_DrugService.Repository
+{
+    public interface IDrug_Repository
+    {
+        DrugDetails searchDrugsByID(int DrugId);
+        DrugDetails searchDrugsByName(string Name);
+
+        IEnumerable<DrugLocation> GetDispatchableDrugStock(int DrugId, string Location);
+    }
+}
